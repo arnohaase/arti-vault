281b3f20c488b1ecea5c6e78268e0a137e2c6fd8
Arno Haase <arno.haase@haase-consulting.com>
2023-11-11 14:37:57 +0100

    file system blob storage

--- src/blob/blob_storage.rs
+++ src/blob/blob_storage.rs
@@ -10,8 +10,6 @@ pub trait BlobStorage<Key: Clone + Debug + Eq + PartialEq + Hash> {
     /// The key for looking up blobs
     async fn insert(&self, data: impl Stream<Item=Bytes> + Send)-> anyhow::Result<Key>;
 
-    async fn update(&self, key: &Key, data: impl Stream<Item=Bytes> + Send) -> anyhow::Result<bool>;
-
     async fn get(&self, key: &Key, ) -> anyhow::Result<Option<Box<dyn Stream<Item = std::io::Result<Bytes>>>>>;
 
     async fn delete(&self, key: &Key) -> anyhow::Result<bool>;
--- src/blob/fs_blob_storage.rs
+++ src/blob/fs_blob_storage.rs
@@ -1,11 +1,10 @@
 use std::path::PathBuf;
 
-use anyhow::anyhow;
 use async_trait::async_trait;
 use bytes::Bytes;
 use futures::StreamExt;
 use futures_core::Stream;
-use tokio::fs::{create_dir_all, metadata, OpenOptions, read_dir, remove_dir, remove_file, rename, try_exists};
+use tokio::fs::{create_dir_all, OpenOptions, read_dir, remove_dir, remove_file, rename, try_exists};
 use tokio::io::AsyncWriteExt;
 use tokio_util::io::ReaderStream;
 use tracing::{error, trace};
@@ -33,7 +32,6 @@ impl FsBlobStorage {
 
     async fn do_insert(
         directory_path: PathBuf,
-        file_name_suffix: &str,
         data: impl Stream<Item=Bytes> + Send
     ) -> anyhow::Result<PathBuf> {
         let mut data = Box::pin(data);
@@ -41,10 +39,8 @@ impl FsBlobStorage {
         //TODO trace
         //TODO performance / monitoring
 
-        //TODO temporary 'dirty' marker in the file system
-
         let mut data_path = directory_path;
-        data_path.push(format!("data{}", file_name_suffix));
+        data_path.push("data");
 
         let mut file = OpenOptions::new()
             .create_new(true)
@@ -64,35 +60,6 @@ impl FsBlobStorage {
 
         Ok(data_path)
     }
-
-    async fn create_lock(directory: &PathBuf) -> anyhow::Result<PathBuf> {
-        let mut path = directory.clone();
-        path.push(".lock");
-
-        if try_exists(&path).await? {
-            let lock_metadata = metadata(path).await?;
-            let lock_time = lock_metadata.created()?; //TODO better write the application's timestamp to the file than use the file system's?
-            let is_timed_out = match lock_time.elapsed() {
-                Ok(duration) => duration.as_secs() > 5*60, //TODO make this configurable
-                Err(_) => false, // lock timestamp is in the future
-            };
-
-            if is_timed_out {
-                //TODO clean up / self heal
-                todo!()
-            }
-            else {
-                return Err(anyhow!("locked / concurrent access"));
-            }
-        }
-
-        OpenOptions::new()
-            .create_new(true)
-            .open(&path)
-            .await?;
-
-        Ok(path)
-    }
 }
 
 
@@ -107,13 +74,18 @@ impl BlobStorage<Uuid> for FsBlobStorage {
         //TODO performance / monitoring
         let key = Uuid::new_v4();
         let directory_path = self.directory_path_for_key(&key);
+
         trace!("inserting file blob - synthetic key is {}, directory is {}", key.as_hyphenated(), directory_path.display());
-        create_dir_all(&directory_path).await?;
 
-        let lock_file = Self::create_lock(&directory_path).await?;
+        let mut temp_directory_path = directory_path.clone();
+        temp_directory_path.pop();
+        temp_directory_path.push(format!("{}.inserting", key.as_hyphenated()));
 
-        let result = match Self::do_insert(directory_path, "", data).await {
+        create_dir_all(&temp_directory_path).await?;
+
+        let result = match Self::do_insert(directory_path.clone(), data).await {
             Ok(_) => {
+                rename(temp_directory_path, directory_path).await?;
                 Ok(key)
             }
             Err(e) => {
@@ -126,63 +98,12 @@ impl BlobStorage<Uuid> for FsBlobStorage {
                 Err(e)
             }
         };
-
-        // no point in propagating a failure to delete the lock file up the call stack
-        let _ = remove_file(&lock_file).await;
         result
-    }
-
-    async fn update(&self, key: &Uuid, data: impl Stream<Item=Bytes> + Send) -> anyhow::Result<bool> {
-        let directory_path = self.directory_path_for_key(key);
-        trace!("updating file system blob {} in directory {}", key.as_hyphenated(), directory_path.display());
-
-        if !try_exists(&directory_path).await? {
-            return Ok(false);
-        }
-
-        let lock_file = Self::create_lock(&directory_path).await?;
-
-        //TODO reject if there is a lock
-        //TODO self-heal if there is an old lock
-
-        // Start by writing the new blob data to a separate file, suffixed ".new".
-        // The idea is to minimize the time of inconsistent state in the file system, and allow full
-        //  recoverability.
-        let new_path = match Self::do_insert(directory_path.clone(), ".new", data).await {
-            Ok(p) => p,
-            Err(e) => {
-                // roll back failed changes as best we can
-                let mut new_file_path = directory_path;
-                new_file_path.push("data.new");
-                let _ = remove_file(new_file_path).await;
-                let _ = remove_file(lock_file).await;
-                return Err(e);
-            }
-        };
-
-        let mut bak_path = directory_path.clone();
-        bak_path.push("data.orig");
-        let mut data_path = directory_path.clone();
-        data_path.push("data");
-
-        //TODO
-        // NB: From this point on, failure requires manual intervention (or at least invasive automation)
-
-        // move original blob data to "data.orig"...
-        rename(&data_path, &bak_path).await?;
-
-        // ... and "data.new" to "data".
-        rename(&new_path, data_path).await?;
-
-        let _ = remove_file(lock_file).await;
-        Ok(true)
     }
 
     async fn get(&self, key: &Uuid) -> anyhow::Result<Option<Box<dyn Stream<Item=std::io::Result<Bytes>>>>> {
         let directory_path = self.directory_path_for_key(key);
         trace!("getting file system blob {} from directory {}", key.as_hyphenated(), directory_path.display());
-
-        //TODO there is a small but non-empty window during an update when the 'get' does not find a data file -> handle
 
         let mut data_path = directory_path;
         data_path.push("data");
